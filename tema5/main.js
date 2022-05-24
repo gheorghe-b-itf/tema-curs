3f86253a6c0557f0bc90f42ead69708c2925de43
Gheorghe <b.gheorghe18@gmail.com>
2022-05-24 22:24:48 +0300

    palindrom checker

--- tema5/main.js
+++ tema5/main.js
@@ -11,7 +11,16 @@ let multiply = function (a, b) {
     return a * b
 }
 let x = prompt('Please give a number for the equation:')
-// let equation = substract(add((divide(4,x),5)),add(divide(multiply(x,(add(substract(34,x),10))),6),x))
-// let equation = divide(multiply(x,(add(substract(34,x),10))),6)
+
 let equation = add(substract(add(divide(4,x),5),divide(multiply(x,(add(substract(34,x),10))),6)),4)
 console.log(equation)
+
+let checkPalindrom =(param)=>{
+    if(`${param}`===param.split('').reverse().join('')){
+        console.log('this is a palindrom')
+    } else {
+        console.log('this is not a palindrom!!')
+    }
+}
+let z=prompt('its a palindrom?')
+let result =checkPalindrom(z)
