79705f90942b47f95743de40a1685dfedef08726
Gheorghe <b.gheorghe18@gmail.com>
2022-06-14 00:02:01 +0300

    refactoring code + restore initial div

--- tema7/main.js
+++ tema7/main.js
@@ -1,10 +1,23 @@
 let divPatrat = document.querySelector('.divPatrat');
 let onDivClick = function () {
-    let changePosition = document.querySelector('.divPatrat')
-    changePosition.style.position = 'fixed';
-    changePosition.style.bottom = '0';
-    changePosition.style.right = '0';
-    changePosition.style.margin = '0px 50px 50px 0px';
+
+    let idDiv = document.getElementById('switchButton');
+    if (!(idDiv.className === 'divPatrat')) {
+        idDiv.className = 'divPatrat'
+    } else {
+        idDiv.className = 'divPatratAbsolut'
+    }
+
 }
 
-divPatrat.addEventListener('click', onDivClick)
+divPatrat.addEventListener('click', onDivClick);
+
+// let divPatrat = document.querySelector('.divPatrat');
+// let onDivClick = function () {
+//     let changePosition = document.querySelector('.divPatrat')
+//     changePosition.style.position = 'absolute';
+//     changePosition.style.bottom = '0';
+//     changePosition.style.right = '0';
+//     changePosition.style.margin = '0px 50px 50px 0px'
+// }
+// divPatrat.addEventListener('click', onDivClick);
