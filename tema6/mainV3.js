556a79996c9557b87a05de607510779a4c79a175
Tudor Goia <mindsetcoach360@gmail.com>
2022-06-14 23:05:34 +0300

    another home work fix

--- tema6/mainV3.js
+++ tema6/mainV3.js
@@ -7,23 +7,27 @@ let createButton = function(name, action) {
 
 let theBody = document.querySelector('body');
 
-let fourthAction = function() {
+let fourthAction = function(e) {
     let fifthButton = createButton('Fifth', function() {alert('stop')});
+    e.currentTarget.removeEventListener('click', fourthAction);
     theBody.appendChild(fifthButton);
 };
 
-let thirdAction = function() {
+let thirdAction = function(e) {
     let fourthButton = createButton('Fourth', fourthAction);
+    e.currentTarget.removeEventListener('click', thirdAction);
     theBody.appendChild(fourthButton);
 };
 
-let secondAction = function() {
+let secondAction = function(e) {
     let thirdButton = createButton('Third', thirdAction);
+    e.currentTarget.removeEventListener('click', secondAction);
     theBody.appendChild(thirdButton);
 };
 
-let firstAction = function() {
+let firstAction = function(e) {
     let secondButton = createButton('Second', secondAction);
+    e.currentTarget.removeEventListener('click', firstAction);
     theBody.appendChild(secondButton);
 };
 
