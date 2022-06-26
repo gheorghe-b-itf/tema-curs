72a268990a06efc837fcd84479a37ccf10c91152
Gheorghe <b.gheorghe18@gmail.com>
2022-06-26 21:53:06 +0300

    raw homework 9

---
+++ tema9/main.js
@@ -0,0 +1,14 @@
+
+let a = 2021
+let b = 6
+let c = 1
+
+
+new Date(a, b, c)
+
+for (new Date(a, b, c); new Date(a, b, c) < new Date();) {
+    
+    console.log(new Date(a, b, c))
+    c+=6;
+    
+}
