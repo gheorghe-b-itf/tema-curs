c7d75fcd3b36e64bcce46e464d5e7424c661ae4e
Gheorghe <b.gheorghe18@gmail.com>
2022-06-27 19:23:06 +0300

    finished HW9

--- tema9/main.js
+++ tema9/main.js
@@ -2,13 +2,19 @@
 let a = 2021
 let b = 6
 let c = 1
-
+let date = ''
 
 new Date(a, b, c)
 
-for (new Date(a, b, c); new Date(a, b, c) < new Date();) {
-    
+for (new Date(a, b, c); new Date(a, b, c) < new Date(); c += 6) {
+
+    date = new Date(a, b, c)
     console.log(new Date(a, b, c))
-    c+=6;
     
+    let scopeDate = document.createTextNode(date)
+    document.getElementById("this").appendChild(scopeDate)
+
+    let br = document.createElement('br')
+    document.getElementById("this").appendChild(br)
+
 }
