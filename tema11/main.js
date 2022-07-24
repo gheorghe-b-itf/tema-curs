7de6c3e1de9c088fac1b4035547c7ef831d342ec
Gheorghe <b.gheorghe18@gmail.com>
2022-07-24 22:00:11 +0300

    half done HW

--- tema11/main.js
+++ tema11/main.js
@@ -1,77 +1,117 @@
 
-let curentDateArr = moment().format("ddd DD M YYYY").split(' ')
-console.log(curentDateArr)
+obj = {
+    allDataLauches: [],
+    dataLauches: {
+        name: [],
+        date_utc: [],
+        details: [],
+        youtubeLink: []
+    },
+    init: function () {
+        axios
+            .get('https://api.spacexdata.com/v4/launches')
+            .then(function(response) {
+                // success
+                obj.allDataLauches = response.data;
+                
+            })
+            .catch(function(error) {
+                // error
+                console.log(error);
+            })
 
-let a = curentDateArr[3];
-let b = curentDateArr[2] - 1;
-let c = curentDateArr[1];
-let date = new Date(a, b, c);
-let dateWrapper = moment(date)._d.toString()
-console.log(dateWrapper)
+            for(let i = 0; i < obj.allDataLauches.length; i++) {
+                obj.dataLauches.name= obj.allDataLauches[i].name
+                date.push(obj.allDataLauches[i].date_utc)
+                obj.dataLauches.details.push(obj.allDataLauches[i].details)
+                obj.dataLauches.youtubeLink.push(obj.allDataLauches[i].links.webcast)
+    
+            }
+        ;
+        let date = []
 
-
-let title = document.querySelector('.title>h1').innerText = moment(new Date(dateWrapper)).format("MMMM")
-
-
-
-let dayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-
-let table = document.querySelector('.tableCalendar')
-let trHead = document.createElement('tr');
-table.appendChild(trHead);
-
-for (let i = 0; i < dayNames.length; i++) {
-    let th = document.createElement('th');
-    th.appendChild(document.createTextNode(dayNames[i]))
-    trHead.appendChild(th)
+        
+        console.log(date)
+    }
 }
+obj.init();
 
-let k = 1;
-let tr = document.createElement('tr');
-let td = document.createElement('td');
-let daysInMonth = moment().daysInMonth()
-let startOfMonth = moment().startOf('month').format('dddd')
+console.log(obj)
 
-table.appendChild(tr);
+let createDynamicCalendar = function () {
 
+    //create a Date obj that I can modify as I need
+    let curentDateArr = moment().format("ddd DD M YYYY").split(' ')
+    let a = curentDateArr[3];
+    let b = curentDateArr[2] - 1;
+    let c = curentDateArr[1];
+    let date = new Date(a, b, c);
 
-while (k <= daysInMonth) {
-    let dayName = moment(new Date(a, b, k)).format('dddd')
+    //create a dynamic title of calendar
+    let dateWrapper = moment(date)._d.toString()
+    let title = document.querySelector('.title>h1').innerText = moment(new Date(dateWrapper)).format("MMMM")
 
-    console.log('')
+    //create header of calendar 
+    let dayNames = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    let gridHeader = document.querySelector('.grid-header')
 
-
-    switch (k) {
-        case 1:
-            for (let i = 0; i < dayNames.length; i++) {
-
-                if (startOfMonth !== dayNames[i]) {
-                    let td = document.createElement('td');
-                    tr.appendChild(td)
-                } else {
-                    let td = document.createElement('td');
-                    td.innerText = '1'
-                    tr.appendChild(td)
-                    break;
-                }
-            }
-            break;
-        default:
-
-            let td = document.createElement('td');
-            td.innerText = k;
-            tr.appendChild(td)
-
-            if (dayName === "Monday") {
-                let tr = document.createElement('tr');
-                let td = document.createElement('td');
-                table.appendChild(tr);
-                td.innerText = k;
-                tr.appendChild(td)
-            }
-            break;
+    for (let i = 0; i < dayNames.length; i++) {
+        let divHead = document.createElement('div');
+        divHead.appendChild(document.createTextNode(dayNames[i]))
+        gridHeader.appendChild(divHead);
+        divHead.classList.add("itemHead");
     }
 
-    k++;
+    //create the actual container of calendar
+    let k = 1;
+    let divContainer = document.querySelector('.grid-container');
+    let daysInMonth = moment(new Date(dateWrapper)).daysInMonth()
+    let startOfMonth = moment(new Date(dateWrapper)).startOf('month').format('dddd')
+
+    while (k <= daysInMonth) {
+
+        // let launchDate = moment(new Date()).format("YYYY MMMM D")
+        switch (k) {
+            case 1:   //create the starting point of calendar
+                for (let i = 0; i < dayNames.length; i++) {
+
+                    if (startOfMonth !== dayNames[i]) {
+                        let divItem = document.createElement('div');
+                        divContainer.appendChild(divItem);
+                        divItem.classList.add("itemCalendar");
+
+                    } else {
+                        let divItem = document.createElement('div');
+                        divItem.innerText = '1'
+                        divContainer.appendChild(divItem);
+                        divItem.classList.add("itemCalendar");
+
+                        // if(launchDate == new Date(a,b,k)) {    //finding launch date
+                        //     let divItemLaunch = document.createElement('div');
+                        //     divItemLaunch.classList.add("itemLaunch");
+                        //     return divItemLaunch;
+                        // }
+                        break;
+                    }
+                }
+                break;
+            default:
+
+                let divItem = document.createElement('div');
+                divItem.innerText = k;
+                divContainer.appendChild(divItem);
+                divItem.classList.add("itemCalendar");
+
+                // if(launchDate == new Date(a,b,k)) {       //finding launch date
+                //     let divItemLaunch = document.createElement('div');
+                //     divItemLaunch.classList.add("itemLaunch");
+                //     return divItemLaunch;
+                // }
+                break;
+        }
+        k++;
+    }
 
 }
+
+createDynamicCalendar();
